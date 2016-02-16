b4ecf4ca183ca64270dff50fc796f8b0297e4fb7
berthelol <loicberthelot1@gmail.com>
2016-02-16 17:21:31 +0100

    First code push

---
+++ RLI-TP2-Elevator-with-MODBUS/RLI - TP2 - Elevator with MODBUS/RLI - TP2 - Elevator with MODBUS/Backup/RLI - TP2 - Elevator/Elevator.vb
@@ -0,0 +1,157 @@
+﻿Imports System.Text
+Imports RLI___TP2___Elevator.AsyncSocket.AsynchronousSocket
+Imports RLI___TP2___Elevator.AsyncSocket.ClientSocket
+Imports RLI___TP2___Elevator.AsyncSocket.ServerSocket
+
+Public Class Elevator
+    Public Shared ServerName As String = "localhost"
+    Private serverIsRunning As Boolean = False
+    Private clientIsRunning As Boolean = False
+
+    Private Sub ConnectToServer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ConnectToServer.Click
+        If Not clientIsRunning Then
+            Me.clientIsRunning = True
+            Dim serverNameForm As AsyncSocket.ServerNameForm = New AsyncSocket.ServerNameForm
+            serverNameForm.ShowDialog()
+            Me.ConnectToServer.ForeColor = System.Drawing.Color.Green
+            Me.ConnectToServer.Text = "Disconnect From the Server"
+
+            Try
+                Me._socket = New AsynchronousClient()
+                _socket.AttachReceiveCallBack(AddressOf ReceivedDataFromServer)
+                TryCast(_socket, AsynchronousClient).ConnectToServer(ServerName)
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+                Me.clientIsRunning = False
+                Me.ConnectToServer.ForeColor = System.Drawing.Color.Red
+                Me.ConnectToServer.Text = "Connect To the Server"
+            End Try
+        Else
+            If _socket IsNot Nothing Then
+                _socket.Close()
+            End If
+            Me.clientIsRunning = False
+            Me.ConnectToServer.ForeColor = System.Drawing.Color.Red
+            Me.ConnectToServer.Text = "Connect To the Server"
+        End If
+    End Sub
+
+    Private Sub LauchServer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LauchServer.Click
+        If Not serverIsRunning Then
+            Me._socket = New AsynchronousServer()
+            Me._socket.AttachReceiveCallBack(AddressOf ReceivedDataFromClient)
+            TryCast(_socket, AsynchronousServer).RunServer()
+
+            Me.serverIsRunning = True
+            Me.LauchServer.ForeColor = System.Drawing.Color.Green
+            Me.LauchServer.Text = "Stop the Server"
+        Else
+            If _socket IsNot Nothing Then
+                _socket.Close()
+            End If
+            Me.serverIsRunning = False
+            Me.LauchServer.ForeColor = System.Drawing.Color.Red
+            Me.LauchServer.Text = "Launch the Server"
+        End If
+    End Sub
+
+    Public Sub New()
+        ' Cet appel est requis par le Concepteur Windows Form.
+        InitializeComponent()
+
+        ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+    End Sub
+
+    Private Sub Ascenseur_FormClosed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        If _socket IsNot Nothing Then
+            _socket.Close()
+        End If
+    End Sub
+
+    Public Sub SendMessageToClient(ByVal msg As Byte())
+        If _socket IsNot Nothing Then
+            If TryCast(_socket, AsynchronousServer) IsNot Nothing Then
+                Me._socket.SendMessage(msg)
+            End If
+        End If
+    End Sub
+
+    Public Sub SendMessageToServer(ByVal msg As Byte())
+        If _socket IsNot Nothing Then
+            If TryCast(_socket, AsynchronousClient) IsNot Nothing Then
+                Me._socket.SendMessage(msg)
+            End If
+        End If
+    End Sub
+
+    ' This delegate enables asynchronous calls for setting
+    ' the property on a Checkbox control.
+    Delegate Sub SetCoilUPCallback(ByVal [val] As Boolean)
+    Public Sub SetCoilUP(ByVal [val] As Boolean)
+        ' InvokeRequired required compares the thread ID of the
+        ' calling thread to the thread ID of the creating thread.
+        ' If these threads are different, it returns true.
+        If Me.CoilUP.InvokeRequired Then
+            Dim d As New SetCoilUPCallback(AddressOf SetCoilUP)
+            Me.Invoke(d, New Object() {[val]})
+        Else
+            Me.CoilUP.Checked = [val]
+        End If
+    End Sub
+
+    ' This delegate enables asynchronous calls for setting
+    ' the property on a Checkbox control.
+    Delegate Sub SetCoilDownCallback(ByVal [val] As Boolean)
+    Public Sub SetCoilDown(ByVal [val] As Boolean)
+        ' InvokeRequired required compares the thread ID of the
+        ' calling thread to the thread ID of the creating thread.
+        ' If these threads are different, it returns true.
+        If Me.CoilDown.InvokeRequired Then
+            Dim d As New SetCoilDownCallback(AddressOf SetCoilDown)
+            Me.Invoke(d, New Object() {[val]})
+        Else
+            Me.CoilDown.Checked = [val]
+        End If
+    End Sub
+
+
+
+
+
+
+
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    ' YOUR JOB START HERE. You don't have to modify another file!
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    Private Sub ReceivedDataFromServer(ByVal sender As Object, ByVal e As AsyncEventArgs)
+        'Add some stuff to interpret messages (and remove the next line!)
+        'Bytes are in e.ReceivedBytes and you can encore the bytes to string using Encoding.ASCII.GetString(e.ReceivedBytes)
+        MessageBox.Show("Server says :" + Encoding.ASCII.GetString(e.ReceivedBytes), "I am Client")
+
+        'BE CAREFUL!! 
+        'If you want to change the properties of CoilUP/CoilDown/LedSensor... here, you must use safe functions. 
+        'Functions for CoilUP and CoilDown are given (see SetCoilDown and SetCoilUP)
+    End Sub
+
+    Private Sub ReceivedDataFromClient(ByVal sender As Object, ByVal e As AsyncEventArgs)
+        'Add some stuff to interpret messages (and remove the next line!)
+        'Bytes are in e.ReceivedBytes and you can encore the bytes to string using Encoding.ASCII.GetString(e.ReceivedBytes)
+        MessageBox.Show("Client says :" + Encoding.ASCII.GetString(e.ReceivedBytes), "I am Server")
+
+        'BE CAREFUL!! 
+        'If you want to change the properties of CoilUP/CoilDown/LedSensor... here, you must use safe functions. 
+        'Functions for CoilUP and CoilDown are given (see SetCoilDown and SetCoilUP)
+    End Sub
+
+
+
+    Private Sub ButtonCallFloor2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonCallFloor2.Click
+        If serverIsRunning Then
+            Me.SendMessageToClient(Encoding.ASCII.GetBytes("Coucou client !"))
+        End If
+        If clientIsRunning Then
+            Me.SendMessageToServer(Encoding.ASCII.GetBytes("Coucou server !"))
+        End If
+    End Sub
+End Class
